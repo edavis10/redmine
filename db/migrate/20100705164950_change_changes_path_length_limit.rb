59905a2340be3712fde95790959ce00161b0ac07
Holger Just <redmine@meine-er.de>
2010-07-06 19:36:33 +0200

    Force the default value of path to be set on the Change model class. This is needed because MySQL does not support default values on text columns (Error introduced in r3828, #5771)

--- db/migrate/20100705164950_change_changes_path_length_limit.rb
+++ db/migrate/20100705164950_change_changes_path_length_limit.rb
@@ -1,9 +1,13 @@
 class ChangeChangesPathLengthLimit < ActiveRecord::Migration
   def self.up
+<<<<<<< HEAD
     # these are two steps to please MySQL 5 on Win32
     change_column :changes, :path, :text, :default => nil, :null => true
     change_column :changes, :path, :text, :null => false
     
+=======
+    change_column :changes, :path, :text, :null => false
+>>>>>>> Force the default value of path to be set on the Change model class. This is needed because MySQL does not support default values on text columns (Error introduced in r3828, #5771)
     change_column :changes, :from_path, :text
   end
 
