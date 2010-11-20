c69b402eec8a7001dd6dfb3660ff4f78956ab145
Toshi MARUYAMA <marutosijp2@gmail.com>
2010-11-21 01:41:36 +0900

    imported patch hg-version-2010-11-20.patch

--- test/unit/lib/redmine/scm/adapters/mercurial_adapter_test.rb
+++ test/unit/lib/redmine/scm/adapters/mercurial_adapter_test.rb
@@ -1,3 +1,4 @@
+
 require File.dirname(__FILE__) + '/../../../../../test_helper'
 begin
   require 'mocha'
@@ -9,14 +10,26 @@ begin
     TEMPLATE_EXTENSION = Redmine::Scm::Adapters::MercurialAdapter::TEMPLATE_EXTENSION
     
     REPOSITORY_PATH = RAILS_ROOT.gsub(%r{config\/\.\.}, '') + '/tmp/test/mercurial_repository'
+
+    def setup
+      @adapter = Redmine::Scm::Adapters::MercurialAdapter
+    end
     
     def test_hgversion
-      to_test = { "Mercurial Distributed SCM (version 0.9.5)\n" => [0,9,5],
-                  "Mercurial Distributed SCM (1.0)\n" => [1,0],
-                  "Mercurial Distributed SCM (1e4ddc9ac9f7+20080325)\n" => nil,
-                  "Mercurial Distributed SCM (1.0.1+20080525)\n" => [1,0,1],
-                  "Mercurial Distributed SCM (1916e629a29d)\n" => nil,
-                  "Mercurial SCM Distribuito (versione 0.9.5)\n" => [0,9,5]}
+      to_test = {
+                    "0.9.5"                 => [0,9,5],
+                    "1.0"                   => [1,0],
+                    "1e4ddc9ac9f7+20080325" => [],
+                    "1.0.1+20080525"        => [1,0,1],
+                    "1916e629a29d"          => [] ,
+                    "1.6"                   => [1,6],
+                    "1.6.1"                 => [1,6,1],
+                    "Mercurial Distributed SCM (version 1.6.3)" => [1,6,3],
+                    ## Italian
+                    # $ make local
+                    # $ LANG=it ./hg --version
+                    "Mercurial SCM Distribuito (versione 1.6.3+61-1c9bb7e00f71)" => [1,6,3],
+                  }
       
       to_test.each do |s, v|
         test_hgversion_for(s, v)
@@ -24,10 +37,14 @@ begin
     end
     
     def test_template_path
-      to_test = { [0,9,5] => "0.9.5",
-                  [1,0] => "1.0",
-                  [] => "1.0",
-                  [1,0,1] => "1.0"}
+      to_test = {
+                    [0,9,5] => "0.9.5",
+                    [1,0]   => "1.0"  ,
+                    []      => "1.0"  ,
+                    [1,0,1] => "1.0"  ,
+                    [1,6]   => "1.0"  ,
+                    [1,6,1] => "1.0"  ,
+                  }
       
       to_test.each do |v, template|
         test_template_path_for(v, template)
@@ -37,18 +54,20 @@ begin
     private
     
     def test_hgversion_for(hgversion, version)
-      Redmine::Scm::Adapters::MercurialAdapter.expects(:hgversion_from_command_line).returns(hgversion)
-      adapter = Redmine::Scm::Adapters::MercurialAdapter
-      assert_equal version, adapter.hgversion
+      @adapter.expects(:hgversion_from_command_line).returns(hgversion)
+      assert_equal version, @adapter.hgversion
     end
     
     def test_template_path_for(version, template)
-      adapter = Redmine::Scm::Adapters::MercurialAdapter
-      assert_equal "#{TEMPLATES_DIR}/#{TEMPLATE_NAME}-#{template}.#{TEMPLATE_EXTENSION}", adapter.template_path_for(version)
-      assert File.exist?(adapter.template_path_for(version))
+      assert_equal "#{TEMPLATES_DIR}/#{TEMPLATE_NAME}-#{template}.#{TEMPLATE_EXTENSION}",
+                   @adapter.template_path_for(version)
+      assert File.exist?(@adapter.template_path_for(version))
     end
   end
   
 rescue LoadError
-  def test_fake; assert(false, "Requires mocha to run those tests")  end
+  class MercurialMochaFake < ActiveSupport::TestCase
+    def test_fake; assert(false, "Requires mocha to run those tests")  end
+  end
 end
+
