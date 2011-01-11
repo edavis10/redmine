59d02deff06f4a4ce35eae0b51892f232a6ebdb7
icy <kyanh@viettug.org>
2011-01-11 20:58:52 +0700

    (find_journal) accepts option :require_edit = true|false to check whether the journal is editable by current user

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -5,12 +5,12 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -32,9 +32,9 @@ class JournalsController < ApplicationController
     retrieve_query
     sort_init 'id', 'desc'
     sort_update(@query.sortable_columns)
-    
+
     if @query.valid?
-      @journals = @query.journals(:order => "#{Journal.table_name}.created_on DESC", 
+      @journals = @query.journals(:order => "#{Journal.table_name}.created_on DESC",
                                   :limit => 25)
     end
     @title = (@project ? @project.name : Setting.app_title) + ": " + (@query.new_record? ? l(:label_changes_details) : @query.name)
@@ -42,7 +42,7 @@ class JournalsController < ApplicationController
   rescue ActiveRecord::RecordNotFound
     render_404
   end
-  
+
   def new
     journal = Journal.find(params[:journal_id]) if params[:journal_id]
     if journal
@@ -56,7 +56,7 @@ class JournalsController < ApplicationController
     text = text.to_s.strip.gsub(%r{<pre>((.|\s)*?)</pre>}m, '[...]')
     content = "#{ll(Setting.default_language, :text_user_wrote, user)}\n> "
     content << text.gsub(/(\r?\n|\r\n?)/, "\n> ") + "\n\n"
-      
+
     render(:update) { |page|
       page.<< "$('notes').value = \"#{escape_javascript content}\";"
       page.show 'update'
@@ -65,7 +65,7 @@ class JournalsController < ApplicationController
       page << "$('notes').scrollTop = $('notes').scrollHeight - $('notes').clientHeight;"
     }
   end
-  
+
   def edit
     if request.post?
       @journal.update_attributes(:notes => params[:notes]) if params[:notes]
@@ -77,11 +77,16 @@ class JournalsController < ApplicationController
       end
     end
   end
-  
+
 private
-  def find_journal
+  # Find journal id. If <tt>require_edit</tt> is true, the journal must
+  # be editable by current user. This is a special re-factor by evadis (?),
+  # but it affects the plugin 'vote': if any actions rely on 'find_journal'
+  # to look up the journal's id (but don't care if the journal is editable),
+  # they may simply fail. -- icy 2010 Dec 26
+  def find_journal(require_edit = true)
     @journal = Journal.find(params[:id])
-    (render_403; return false) unless @journal.editable_by?(User.current)
+    (render_403; return false) if require_edit and not @journal.editable_by?(User.current)
     @project = @journal.journalized.project
   rescue ActiveRecord::RecordNotFound
     render_404
