6a37ddb242b444ee1d4415ff9bbe1a9515a0225a
Akinori MUSHA <knu@idaemons.org>
2011-06-01 16:08:53 +0900

    Fix a double-escape problem in auto_link_urls that persists in Rails 2.3.5.

--- config/initializers/10-patches.rb
+++ config/initializers/10-patches.rb
@@ -60,6 +60,35 @@ module ActionView
         end
       end
     end
+
+    module TextHelper
+      def auto_link_urls(text, html_options = {})
+        link_attributes = html_options.stringify_keys
+        text.gsub(AUTO_LINK_RE) do
+          href = $&
+          punctuation = ''
+          left, right = $`, $'
+          # detect already linked URLs and URLs in the middle of a tag
+          if left =~ /<[^>]+$/ && right =~ /^[^>]*>/
+            # do not change string; URL is alreay linked
+            href
+          else
+            # don't include trailing punctuation character as part of the URL
+            if href.sub!(/[^\w\/-]$/, '') and punctuation = $& and opening = BRACKETS[punctuation]
+              if href.scan(opening).size > href.scan(punctuation).size
+                href << punctuation
+                punctuation = ''
+              end
+            end
+
+            link_text = block_given?? yield(href) : href
+            href = 'http://' + href unless href.index('http') == 0
+
+            content_tag(:a, link_text, link_attributes.merge('href' => href)) + punctuation
+          end
+        end
+      end
+    end if Rails::VERSION::MAJOR <= 2
   end
 end
 
